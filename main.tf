779ca89d1cb4a3f4eb6bcb0a4e18be0b4c4185b1
Deeptiitha23 <158009664+Deeptiitha23@users.noreply.github.com>
2024-05-30 12:20:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "example1"{
         provider=aws.insta1
         key_name="test"
         tags={
-        Name="master2"
+        Name="master3"
         }
 }
 resource "aws_instance" "example2"{
@@ -21,7 +21,7 @@ resource "aws_instance" "example2"{
         provider=aws.insta2
         key_name="test"
         tags={
-        Name="worker2"
+        Name="worker3"
         }
 }
 output "private_ips"{
